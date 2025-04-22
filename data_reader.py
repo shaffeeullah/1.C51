b037dfb2963cc218fa92df667f672567d4e7c0d1
Sameena Shaffeeullah <samazisha@gmail.com>
2025-04-22 11:43:32 -0400

    working reader

--- data_reader.py
+++ data_reader.py
@@ -1,7 +1,9 @@
 import xarray as xr
 
-# Load GRIB using cfgrib engine
-ds = xr.open_dataset("data/data.grib", engine="cfgrib")
-print(ds)
-df = ds.to_dataframe().reset_index()
+# Load GRIB using cfgrib engine with chunking
+ds = xr.open_dataset("data/data.grib", engine="cfgrib", chunks={'time': 100})
+# Select only the first few time steps
+ds_subset = ds.isel(time=slice(0, 2))
+print(ds_subset)
+df = ds_subset.to_dataframe().reset_index()
 print(df.head())
